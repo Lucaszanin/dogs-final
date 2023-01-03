0acf836068b93db581ac9211fac9ba98f7bd0667
Lucas Zanin <lucas.zanin@agenciam3.com>
2023-01-03 08:54:04 -0300

    feat(useForm):criando hook para validação dos inputs

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,37 +1,40 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "../../Hooks/useForm";
 import Button from "../Form/Button";
 import Input from "../Form/Input";
 import URL from "../Url";
 
 function LoginForm() {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const username = useForm();
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch(`${URL}/jwt-auth/v1/token`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userName, password }),
-    })
-      .then((res) => {
-        console.log(res);
-        return res.json();
+    if (username.validate() && password.validate()) {
+      fetch(`${URL}/jwt-auth/v1/token`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(),
       })
-      .then((json) => {
-        console.log(json);
-      });
+        .then((res) => {
+          console.log(res);
+          return res.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
