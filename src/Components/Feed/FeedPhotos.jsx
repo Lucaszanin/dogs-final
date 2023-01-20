12fba776deb94140de1a88436a17c3ea85c5c160
Lucas Zanin <lucas.zanin@agenciam3.com>
2023-01-19 23:13:43 -0300

    feat(loading,scrollinfinito): criando lógica do scroll infinito e do loading da página

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,23 +1,24 @@
 import { useEffect } from "react";
-import FeedPhotosItem from "./FeedPhotosItem";
-import useFetch from "../../Hooks/useFetch";
 import { PHOTOS_GET } from "../../api";
+import useFetch from "../../Hooks/useFetch";
 import { Error } from "../Helper/Error";
 import Loading from "../Helper/Loading";
 import styles from "./FeedPhotos.module.css";
+import FeedPhotosItem from "./FeedPhotosItem";
 
-function FeedPhotos({ setModalPhoto }) {
+function FeedPhotos({ page, user, setModalPhoto, setInfinite }) {
   const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const total = 3;
+      const { url, options } = PHOTOS_GET({ page, total, user });
       const { response, json } = await request(url, options);
-      console.log(response, json);
+      if (response && response.ok && json.length < total) setInfinite(false);
     }
 
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
---
+++ src/Components/Helper/Image.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import styles from "./Image.module.css";
+
+function Image({ alt, ...props }) {
+  const [skeleton, setSkeleton] = useState(true);
+
+  function handleLoad({ target }) {
+    setSkeleton(false);
+    target.style.opacity = 1;
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      {skeleton && <div className={styles.skeleton}></div>}
+      <img alt={alt} className={styles.img} {...props} onLoad={handleLoad} />
+    </div>
+  );
+}
+
+export default Image;
